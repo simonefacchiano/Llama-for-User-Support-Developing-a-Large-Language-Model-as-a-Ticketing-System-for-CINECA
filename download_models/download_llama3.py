a77022dc025fa2ec3df035f1b7b6a4d7efb6e928
simonefacchiano <facchiano.1919922@studenti.uniroma1.it>
2024-10-10 15:30:30 +0200

    Modifica a downlaod_llama3.py

--- download_models/download_llama3.py
+++ download_models/download_llama3.py
@@ -1,3 +1,6 @@
+# This file was only used once to download Llama 3 from Hugging Face.
+# To do so, we first asked Meta for the permissione, which required a couple of hours to get accepted.
+
 import os
 
 os.system("huggingface-cli login --token $HUGGINGFACE_TOKEN")
