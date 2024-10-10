b2840beca202b66a09c987d4e7e9bf8c0c08dfd7
simonefacchiano <facchiano.1919922@studenti.uniroma1.it>
2024-10-10 15:23:51 +0200

    Modifica a download_model.py

--- download_models/download_model.py
+++ download_models/download_model.py
@@ -1,19 +1,16 @@
-# File generico per installare un nuovo modello
+# Thanks to this file, you can install a new model.
+# We used this for downloading the embedding model we used in RAG.
 
 import os
 
-# Definisci il percorso della cartella dove vuoi scaricare il modello
-model_dir = "/leonardo_work/try24_facchian/prove/embedding_model_new"  # Sostituisci con il percorso desiderato
+model_dir = "/leonardo_work/try24_facchian/prove/embedding_model_new"  # <-- Change here
 
 # Crea la cartella se non esiste
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
-# Login in Hugging Face
+# Login in Hugging Face, la chiave è stata definita come variabile d'ambiente
 os.system("huggingface-cli login --token $HUGGINGFACE_TOKEN")
 
-# Scarica il modello nella cartella specificata
-os.system(f"huggingface-cli download openbmb/MiniCPM-Embedding --local-dir {model_dir}")
-
-# intfloat/multilingual-e5-base --> quello utilizzato inizialmente
-# openbmb/MiniCPM-Embedding --> quello nuovo
+# Scarica il modello
+os.system(f"huggingface-cli download intfloat/multilingual-e5-base --local-dir {model_dir}")
